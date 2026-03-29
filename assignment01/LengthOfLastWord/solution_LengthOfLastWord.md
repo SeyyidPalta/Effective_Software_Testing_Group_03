871f628869258080183be478427a6d228119b533
SeyyidPalta <seyyidoekkes.palta@uzh.ch>
2026-03-29 19:17:46 +0200

    finish LengthOfLastWord

--- assignment01/LengthOfLastWord/solution_LengthOfLastWord.md
+++ assignment01/LengthOfLastWord/solution_LengthOfLastWord.md
@@ -4,12 +4,20 @@
 Read the problem description carefully to understand the requirements and expected inputs and outputs.
 
 ## 1.2 Explore the Program
+What happens if the string is empty?
+- It returns 0 because there is no last word.
+
+What happens if there are some spaces after the last word?
+- Spaces are ignored and the length of the last word is returned.
+
+What happens if there are only spaces before the last word?
+- Spaces are ignored yet again and the length of the last word is returned.
 
 ## 1.3 Identify the Partitions
-
+We can either have an empty string. A string with one word and a string with multiple words.
 
 ## 1.4 Analyze the Boundaries
-
+The program is case-insensitive as the case doesn't play a role, and it counts the number of letters in a word.
 
 ## 1.5 Devise Test Cases
 Created test cases for each partition and boundary identified in the previous steps.
@@ -18,9 +26,12 @@ Created test cases for each partition and boundary identified in the previous st
 Created automated tests for each test case using JUnit.
 
 ## 1.7 Augmentation of Test Suites
+Added tests for tricky parts such as spaces after the last word and a punctuation. 
+The program counts punctuations as part of the last word.
 
 
 # 2. Structural Tests
-
+Line coverage is 91%. The only line that is not covered by tests is the class header line (3)
 
 # 3. Mutation Tests
+One mutant could not be killed because the condition where i equals to 0 and also character at that location being " " could not be satisfied.
--- assignment01/LengthOfLastWord/src/test/java/zest/LengthOfLastWordTest.java
+++ assignment01/LengthOfLastWord/src/test/java/zest/LengthOfLastWordTest.java
@@ -1,5 +1,65 @@
 package zest;
 
-public class LengthOfLastWordTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static zest.LengthOfLastWord.lengthOfLastWord;
+
+public class LengthOfLastWordTest {
+    @Test
+    void testBasic() {
+        assertEquals(5, lengthOfLastWord("Hello World"));
+    }
+
+    @Test
+    void testTrailingSpaces() {
+        assertEquals(5, lengthOfLastWord("Hello World   "));
+    }
+
+    @Test
+    void testSingleWord() {
+        assertEquals(5, lengthOfLastWord("Hello"));
+    }
+
+    @Test
+    void testMultipleWords() {
+        assertEquals(7, lengthOfLastWord("Effective Software Testing"));
+    }
+
+    @Test
+    void testPeriodEnd() {
+        assertEquals(8, lengthOfLastWord("Effective Software Testing."));
+    }
+
+    @Test
+    void testNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> lengthOfLastWord(null));
+    }
+
+    @Test
+    void testEmpty() {
+        assertEquals(0, lengthOfLastWord(""));
+    }
+
+    @Test
+    void TestSingleLetter() {
+        assertEquals(1, lengthOfLastWord("a"));
+    }
+
+    @Test
+    void TestEmptyStringWithSpace() {
+        assertEquals(0, lengthOfLastWord(" "));
+    }
+
+    @Test
+    void TestSingleLetterWithSpaceBefore() {
+        assertEquals(1, lengthOfLastWord(" a"));
+    }
+
+    @Test
+    void TestSingleLetterWithSpaceAfter() {
+        assertEquals(1, lengthOfLastWord("a   "));
+    }
 }
