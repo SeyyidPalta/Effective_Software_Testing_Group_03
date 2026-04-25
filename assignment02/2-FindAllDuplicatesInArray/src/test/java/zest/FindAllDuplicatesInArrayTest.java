a81ff94fcc5c6f57a71313a5b31432f62c49649b
SeyyidPalta <seyyidoekkes.palta@uzh.ch>
2026-04-25 21:58:51 +0200

    add tests for task 2

--- assignment02/2-FindAllDuplicatesInArray/src/test/java/zest/FindAllDuplicatesInArrayTest.java
+++ assignment02/2-FindAllDuplicatesInArray/src/test/java/zest/FindAllDuplicatesInArrayTest.java
@@ -1,5 +1,45 @@
 package zest;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FindAllDuplicatesInArrayTest {
 
+    protected FindAllDuplicatesInArray testInstance;
+
+    @BeforeEach
+    public void setUp() {
+        try {
+            testInstance = new FindAllDuplicatesInArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @AfterEach
+    public void tearDown() {
+        try {
+            testInstance = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testEmptyFindDuplicates() {
+        int[] testList = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> expected = List.of();
+        assertEquals(expected, testInstance.findDuplicates(testList));
+    }
+
+    @Test
+    public void testFindDuplicates() {
+        int[] testList = {1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 9};
+        List<Integer> expected = List.of(2, 9);
+        assertEquals(expected, testInstance.findDuplicates(testList));
+    }
 }
