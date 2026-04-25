34f725cf53fd049fd707712413c909586182897d
SeyyidPalta <seyyidoekkes.palta@uzh.ch>
2026-04-25 21:23:49 +0200

    add exercise 1 for assignment 02

--- assignment02/1-MinCostClimbingStairs/src/test/java/zest/MinCostClimbingStairsTest.java
+++ assignment02/1-MinCostClimbingStairs/src/test/java/zest/MinCostClimbingStairsTest.java
@@ -1,5 +1,30 @@
 package zest;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MinCostClimbingStairsTest {
+
+    MinCostClimbingStairs testInstance;
+
+    @BeforeEach
+    void setUp() {
+        testInstance = new MinCostClimbingStairs();
+    }
+    @AfterEach
+    void tearDown() {
+        testInstance = null;
+    }
+
+    @Test
+    void testMinCostClimbingStairs() {
+        int[] cost = {10, 15, 20};
+        int expected = 15;
+        int actual = testInstance.minCostClimbingStairs(cost);
+        assertEquals(expected, actual);
+    }
 
 }
